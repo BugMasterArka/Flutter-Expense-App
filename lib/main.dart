21d8c4d957981845e6cd88923a82b9dc3c0fd220
BugMasterArka <arkalodh100@gmail.com>
2023-02-21 15:53:29 +0530

    made app only work in portrait mode 124

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:expense_app/widgets/chart.dart';
+import './widgets/chart.dart';
+import 'package:flutter/services.dart';
 
 import './widgets/transaction_list.dart';
 import './widgets/new_transaction.dart';
@@ -6,6 +7,11 @@ import 'package:flutter/material.dart';
 import './models/transaction.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
